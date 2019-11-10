503ff6465c614c383f4e1e1a78394cee9b8c6323
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2019-11-10 10:20:09 +0000

    Dropbox-like navbar added for large screen - needs MQ for mobile

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+// debounce helper
+function debounce(func, wait = 15, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+// change button on scroll
+function changeButton(e) {
+  const button = document.querySelector(".nav-button");
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    button.classList.add("active", "has-shadow");
+    navbar.classList.add("active");
+    navbar.classList.add("has-shadow");
+  } else {
+    button.classList.remove("active");
+    navbar.classList.remove("active");
+    navbar.classList.remove("has-shadow");
+  }
+  if (window.scrollY > 200) {
+    button.classList.remove("active");
+    button.classList.add("active-second");
+  } else {
+    button.classList.remove("active-second");
+  }
+  console.log(window.scrollY);
+}
+
+window.addEventListener("scroll", debounce(changeButton));
